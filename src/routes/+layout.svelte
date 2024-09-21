b9a662cee7c1cf0bd546ffc0258ea3b2a480b4ee
Ayush Jhunjhunwala <ayushjhun13@gmail.com>
2024-09-21 19:27:46 +0530

    mobile header added to the layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,12 @@
   import "../app.css";
   import DesktopHeader from "../lib/DesktopHeader.svelte";
   import Footer from "../lib/Footer.svelte";
+  import MobileHeader from "../lib/MobileHeader.svelte";
 </script>
 
 <div class="bg-[#99edff]">
   <DesktopHeader />
+  <MobileHeader />
 </div>
 <div>
   <slot />
