9d62ac278103b136e5a85496d9a077f6a9bad749
Ayush Jhunjhunwala <ayushjhun13@gmail.com>
2024-09-22 19:17:05 +0530

    added some thing to the footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -16,3 +16,10 @@
 <div class="bg-[#99EDFF]">
   <Footer />
 </div>
+<div class="flex h-2 w-full justify-end">
+  <div class="h-full w-[5%] bg-[#AB77FF]"></div>
+  <div class="h-full w-[40%] bg-[#99EDFF]"></div>
+  <div class="h-full w-[7%] bg-[#AB77FF]"></div>
+  <div class="h-full w-[25%] bg-black"></div>
+  <div class="h-full w-[10%] bg-[#ADFF00]"></div>
+</div>
