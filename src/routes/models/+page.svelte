c94ac9beb53be177780fc92ecc7c567e83b765d6
Ayush Jhunjhunwala <ayushjhun13@gmail.com>
2024-09-22 19:17:21 +0530

    explore page added

---
+++ src/routes/models/+page.svelte
@@ -0,0 +1,83 @@
+<div class="container mx-auto px-4 py-6">
+  <div
+    class="mb-4 flex flex-col justify-between space-y-4 md:mb-8 md:flex-row md:items-end lg:mb-12"
+  >
+    <div class="flex-none">
+      <div class="space-y-2">
+        <div class="flex items-center">
+          <h2 class="text-2xl font-medium text-content-strong">
+            Model Gallery
+          </h2>
+        </div>
+        <div class="text text-content-light">
+          See all available model APIs provided by <span
+            class="text-content-strong">fal.ai</span
+          >
+        </div>
+      </div>
+    </div>
+    <div class="flex w-full flex-1 items-end justify-end">
+      <div class="space-x-2">
+        <span>Can't find a model?</span><a
+          href="/models"
+          class="inline-flex items-center justify-center whitespace-nowrap rounded border font-heading font-medium transition-all focus-ring disabled:pointer-events-none disabled:opacity-50 bg-transparent text-content-strong border-stroke-strong hover:text-content-stronger hover:border-stroke-stronger data-[state=open]:bg-surface-alpha-light h-10 px-4"
+          >Suggest Model</a
+        >
+      </div>
+    </div>
+  </div>
+
+  <div class="mb-4 overflow-visible md:mb-8 lg:mb-12">
+    <div
+      class="overflow-hidden rounded-sm bg-gradient-to-r from-theme to-stroke-base to-50% p-px"
+    >
+      <div class="flex flex-row overflow-hidden bg-[#99EDFF]">
+        <div class="flex flex-col space-y-2 p-4 md:max-w-[40%]">
+          <div class="mb-2">
+            <div
+              class="inline-flex items-center rounded border font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 border-transparent px-2 py-0.5 text-sm bg-black text-white"
+            >
+              Available now
+            </div>
+          </div>
+          <h3
+            class="text-xl text-content-strong dark:text-content-inverse-strong md:text-2xl lg:text-3xl"
+          >
+            Flux is here!
+          </h3>
+          <p
+            class="text-sm font-light text-content-base dark:text-content-inverse md:text-base"
+          >
+            Discover the latest in text-to-image technology with enhanced
+            multi-subject capabilities, improved image quality, and better
+            spelling accuracy.
+          </p>
+          <div class="flex gap-4 pt-4 lg:pt-8">
+            <a
+              class="inline-flex bg-black text-white items-center justify-center whitespace-nowrap rounded border font-heading font-medium transition-all focus-ring disabled:pointer-events-none disabled:opacity-50 border-stroke-strong hover:border-stroke-stronger data-[state=open]:bg-surface-alpha-light h-10 px-4 hover:text-content-inverse-stronger bg-content-strong text-content-inverse-strong dark:bg-content-inverse-strong dark:text-content-strong"
+              href="/models">Try it now!</a
+            ><a
+              class="inline-flex items-center border-black justify-center whitespace-nowrap rounded border font-heading font-medium transition-all focus-ring disabled:pointer-events-none disabled:opacity-50 bg-transparent hover:border-stroke-stronger data-[state=open]:bg-surface-alpha-light h-10 px-4 border-content-base text-content-strong hover:text-content-stronger dark:border-content-inverse dark:text-content-inverse-strong"
+              href="/models/">Train a Flux LoRA</a
+            >
+          </div>
+        </div>
+        <div class="flex flex-1 justify-end">
+          <div
+            class="banner-bg relative overflow-clip rounded-r-xl flux-banner-bg"
+          >
+            <div
+              class="absolute left-0 top-0 h-full w-1/3 bg-gradient-to-r from-theme to-transparent"
+            ></div>
+          </div>
+        </div>
+      </div>
+      <div class="flex h-2 w-full flex-row-reverse justify-end">
+        <div class="h-full flex-1 bg-[#99EDFF]"></div>
+        <div class="h-full w-[7%] bg-[#AB77FF]"></div>
+        <div class="h-full w-[25%] bg-black"></div>
+        <div class="h-full w-[10%] bg-[#ADFF00]"></div>
+      </div>
+    </div>
+  </div>
+</div>
